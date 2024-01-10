a88516693735c71f782a61651ba476b0150568e8
emrebulak <emrbrnn@gmail.com>
2024-01-10 17:53:03 +0300

    10.01.2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { NavComponent } from './nav/nav.component';
+declare var $: any;
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FontAwesomeModule, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'e-commorce-app';
+  faPaperPlane = faPaperPlane;
+
+  ngOnInit() {
+  }
+
+  try(){
+    console.log("try");
+    $('.deneme').css('color', 'red');
+  }
 }
